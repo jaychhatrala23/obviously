7ae0854a109e9059840a2f23a08422de57f97ebb
Jay Chhatrala <jay.chhatrala@hotmail.com>
2024-07-06 07:10:53 +0100

    Sqlite in memory db

--- obviously/settings.py
+++ obviously/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "person"
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = "obviously.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'NAME': ':memory:',
     }
 }
 
