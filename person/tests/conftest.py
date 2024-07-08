8435108ce45f70d94f13d44259c37e83df8af92a
Jay Chhatrala <jay.chhatrala@hotmail.com>
2024-07-08 08:16:05 +0100

    Update pytest fixture to use roles from data migration script

--- person/tests/conftest.py
+++ person/tests/conftest.py
@@ -7,7 +7,8 @@ def roles():
     """
     Fixture to create 'admin' and 'guest' roles before running any tests.
     """
-    # Create admin and guest roles
-    admin_role = Role.objects.create(name="admin")
-    guest_role = Role.objects.create(name="guest")
+    # As we have a data migration which is loaded on running the server or starting tests
+    # we are fetching the roles here, otherwise create those roles to be used in tests
+    admin_role = Role.objects.get(name="admin")
+    guest_role = Role.objects.get(name="guest")
     return {"admin": admin_role, "guest": guest_role}
