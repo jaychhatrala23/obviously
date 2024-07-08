075b5eb1ef8ee19cf9609aa406c04c7a44439f01
Jay Chhatrala <jay.chhatrala@hotmail.com>
2024-07-08 08:24:12 +0100

    Update to run migrations on each server / tests run

--- obviously/wsgi.py
+++ obviously/wsgi.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "obviously.settings")
 
 application = get_wsgi_application()
+
+# As we are using an in-memory db, we need to run migrations each time we start the server / tests
+call_command("makemigrations")
+call_command("migrate")
